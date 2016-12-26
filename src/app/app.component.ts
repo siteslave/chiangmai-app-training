16a5691332f8d48c53339c717debfd1ea57fa2ed
Satit Rianpit <rianpit@gmail.com>
2016-12-26 08:33:47 +0700

    remove unused component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform, Events } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 
@@ -23,7 +22,6 @@ export class MyApp {
       else this.rootPage = LoginPage;
 
       events.subscribe('logout', () => {
-        console.log('Logout');
         localStorage.removeItem('token');
         this.rootPage = LoginPage;
       });
